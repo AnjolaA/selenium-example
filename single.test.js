d93c4b45ffa266ca0acdf2bfe83b55e8877a00b1
Anjola <anjola@outlook.com>
2022-01-23 14:48:19 +0000

    added 2 more tests with Abi

--- single.test.js
+++ single.test.js
@@ -13,7 +13,30 @@ describe("Checking the title of the page for BBC", () => {
   }, 15000);
 
   test("it performs a validation of title on the home page", async () => {
+    const liveText = await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div/div/main/div[1]/div[2]/div/div/ul/li[1]/div/div/div[1]/div[1]/a/span/p/span"
+        )
+      )
+      .getText();
+    expect(liveText).toBe("Latest from three Premier League matches");
+  });
+
+  test("it performs a validation of title on the home page 2", async () => {
+    const sola = await driver.getTitle();
+    expect(sola).toContain("BBC - Home");
+  });
+
+  test("it checks the page title of the sports page", async () => {
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div/div/header/div[2]/nav/div[1]/div/div[1]/ul[2]/li[5]/a"
+        )
+      )
+      .click();
     const title = await driver.getTitle();
-    expect(title).toContain("BBC - Home");
+    expect(title).toContain("Home - BBC Sport");
   });
 });
